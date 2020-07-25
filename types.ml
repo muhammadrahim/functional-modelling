f7ae2133d626683fe62812f41ff572fd0e653cb8
Mo <rahim.muhammad@pm.me>
2020-07-25 22:45:29 +0200

    added more types

--- types.ml
+++ types.ml
@@ -1,13 +1,36 @@
-type t = { value : string }
-
+type product_code = ProductCode of string
 type person = { first: string; last: string }
-let aPerson : person = { first = "Alex"; last = "Adams"}
-let last = aPerson.last
+type order_quantity = UnitQuantity of int | KilogramQuantity of float
+type check_number = CheckNumber of int
+type card_number = CardNumber of string
+type card_type = Visa | Mastercard
+type credit_card_info = {
+  card_type : card_type;
+  card_number : card_number;
+}
+type payment_method = Cash | Check of check_number | Card of credit_card_info
+type payment_amount = PaymentAmount of float
+type currency = GBP | EUR
+type payment = {
+  amount: payment_amount;
+  currency : currency;
+  payment_method : payment_method;
+  }
+type unpaid_invoice = UnpaidInvoice
+type paid_invoice = PaidInvoice
+type invoice = unpaid_invoice -> payment -> paid_invoice
+type currency_conversion = payment -> currency -> payment
+type personal_name = {
+  first_name : string;
+  middle_name : string option;
+  last_name : string;
+}
 
-let { first=first_name; last=last_name; } = aPerson
+let test : credit_card_info = { card_type = Visa; card_number = CardNumber "1" }
 
-
-class istack = object
-  val aPerson : person = { first = "Alex"; last = "Adams"}
-end
-
+let anOrderInUnits = UnitQuantity 1
+let anOrderInKg = KilogramQuantity 10.
+let printQty aOrderQty = match aOrderQty with
+  | UnitQuantity uQty ->  Printf.printf "%i units" uQty
+  | KilogramQuantity kgQty -> Printf.printf "%g units" kgQty 
+ 
