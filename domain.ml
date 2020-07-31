abbd94d2e6b53fcb04669e7070629996e93aac96
Mo <rahim.muhammad@pm.me>
2020-07-31 15:17:37 +0200

    modified entity

--- domain.ml
+++ domain.ml
@@ -1,12 +1,21 @@
 type widget_code = WidgetCode of string
 type gizmo_code = GizmoCode of string
 
-type unit_quantity = UnitQuantity of int
+module UnitQuantity = struct
+  type t = int
+  let create value: t = if value < 0 || value > 1000 
+    then failwith (Printf.sprintf "Not in interval")
+else value
+end
+
+type unit_quantity = UnitQuantity
 type kilogram_quantity = KilogramQuantity of float
 type product_code = Widget of widget_code | Gizmo of gizmo_code
 type order_quantity = 
   | UnitQuantity of unit_quantity
   | KilogramQuantity of kilogram_quantity
+
+let test = UnitQuantity.create 1
 
 type undefined = exn
 
@@ -21,12 +30,17 @@ type price = undefined
 type order_line = undefined
 type amount_to_bill = undefined
 
+type ('a) non_empty_list = {
+  first: 'a;
+  rest: 'a list
+}
+
 type order = { 
   id: order_id;
   customer_id : customer_id;
   shipping_address: shipping_address;
   billing_address: billing_address;
-  orderlines: order_line list;
+  orderlines: order_line non_empty_list;
   amount_to_bill: amount_to_bill
 }
 and order_line = {
@@ -72,8 +86,18 @@ type calculate_prices = order_form -> product_catalog -> priced_order
 type contact_id = ContactId of int
 type phone_number = undefined
 type email_address = undefined
+type verified_email_address = undefined
+
+type customer_email = Unverified of email_address | Verified of verified_email_address
+type customer_address = CustomerAddress of string
+type both_contact_methods = { email: verified_email_address; address: customer_address }
+
+type contact_info = 
+  | EmailOnly of verified_email_address
+  | AddrOnly of customer_address
+  | EmailAndAddr of both_contact_methods
+
 type contact = {
   contact_id : contact_id;
-  phone_number : phone_number;
-  email_address : email_address
+  contact_info : contact_info
 }
