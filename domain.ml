e3b98bd188c98eb050fff8b52b518da2307da652
Mo <rahim.muhammad@pm.me>
2020-09-10 10:11:47 +0200

    stricter typing

--- domain.ml
+++ domain.ml
@@ -28,6 +28,7 @@ type shipping_address = undefined
 type billing_address = undefined
 type price = undefined
 type order_line = undefined
+type priced_order_line = undefined
 type amount_to_bill = undefined
 
 type ('a) non_empty_list = {
@@ -80,7 +81,15 @@ type categorized_mail = Quote of quote_form | Order of order_form
 type categorize_inbound_mail = envelope_contents -> categorized_mail
 
 type product_catalog = undefined
-type priced_order = undefined
+type billing_amount = float
+type priced_order = {
+  order_id : order_id;
+  customer_info : customer_info;
+  shipping_address : shipping_address;
+  billing_address : billing_address;
+  orderlines : priced_order_line list;
+  amount_to_bill : billing_amount
+}
 type calculate_prices = order_form -> product_catalog -> priced_order
 
 type contact_id = ContactId of int
@@ -101,3 +110,62 @@ type contact = {
   contact_id : contact_id;
   contact_info : contact_info
 }
+
+type order = 
+  | PricedOrder of priced_order
+  | ValidatedOrder of validated_order 
+  | UnvalidatedOrder of unvalidated_order
+
+type item = undefined
+type active_cart_data = { unpaidItems : item list }
+type paid_cart_data = { paidItems : item list; payment: float }
+
+type shopping_cart =
+  | EmptyCart
+  | ActiveCart of active_cart_data
+  | PaidCart of paid_cart_data
+
+
+type check_product_code_exists = product_code -> bool
+type address_validation_error = AddressValidationError of string
+(* type check_address_exists = unvalidated_address -> (checked_address, address_validation_error) result *)
+
+type get_product_price = product_code -> price
+
+type order_acknowledgement_sent = {
+  order_id : order_id;
+  email_address : email_address
+}
+
+type create_order_acknowledgement_letter
+type send_order_acknowledgement
+type acknowledge_order = create_order_acknowledgement_letter 
+  -> send_order_acknowledgement
+  -> priced_order
+  -> order_acknowledgement_sent option
+
+(* let place_order unvalidated_order =
+  unvalidated_order
+  |> validate_order
+  |> price_order
+  |> acknowledge_order
+  |> create_events *)
+type order_id = OrderId of string
+
+module Domain = struct
+  type order_id = OrderId of string
+  module OrderId = struct
+    let create str: order_id  = OrderId str
+    let value (OrderId str) = str
+  end
+end
+
+let example = Domain.OrderId.create "test";;
+let orig_string = Domain.OrderId.value example
+
+(* module UnitQuantity = struct
+  type t = int
+  let create value: t = if value < 0 || value > 1000 
+    then failwith (Printf.sprintf "Not in interval")
+else value
+end *)
